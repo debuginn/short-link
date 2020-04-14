2803c20c0be4b72c46d2684c82788101842ffd04
debuginn <xianhui.meng.cn@gmail.com>
2020-04-14 18:50:51 +0800

    add file 将 Response 请求集中处理

--- controller/UserController.go
+++ controller/UserController.go
@@ -9,7 +9,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 	"log"
-	"net/http"
 )
 
 // 注册用户
@@ -21,17 +20,11 @@ func Register(context *gin.Context) {
 
 	// 数据验证
 	if len(telephone) != 11 {
-		context.JSON(http.StatusUnprocessableEntity, gin.H{
-			"code": 422,
-			"msg":  "手机号必须为 11位",
-		})
+		response.UnprocessableEntity(context, nil, "手机号必须为 11位")
 		return
 	}
 	if len(password) <= 6 {
-		context.JSON(http.StatusUnprocessableEntity, gin.H{
-			"code": 422,
-			"msg":  "密码不能少于 6位",
-		})
+		response.UnprocessableEntity(context, nil, "密码不能少于 6位")
 		return
 	}
 	if len(username) == 0 {
@@ -39,20 +32,14 @@ func Register(context *gin.Context) {
 	}
 	// 判断手机号是否占用
 	if isTelephoneExist(telephone) {
-		context.JSON(http.StatusUnprocessableEntity, gin.H{
-			"code": 422,
-			"msg":  "用户已经存在",
-		})
+		response.UnprocessableEntity(context, nil, "用户已经存在")
 		return
 	}
 
 	// 创建用户
 	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{
-			"code": 500,
-			"msg":  "加密密码错误",
-		})
+		response.Fail(context, nil, "加密密码错误")
 		return
 	}
 
@@ -64,15 +51,9 @@ func Register(context *gin.Context) {
 
 	errCreate := model.CreateAUser(&newUser)
 	if errCreate != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code": 201,
-			"msg":  "用户创建失败",
-		})
+		response.Created(context, nil, "用户创建失败")
 	} else {
-		context.JSON(http.StatusOK, gin.H{
-			"code": 200,
-			"msg":  "用户创建成功",
-		})
+		response.Success(context, nil, "用户创建成功")
 	}
 }
 
@@ -81,45 +62,44 @@ func Login(context *gin.Context) {
 	// 获取参数
 	telephone := context.PostForm("telephone")
 	password := context.PostForm("password")
-
 	// 数据验证
 	if len(telephone) != 11 {
-		response.Response(context, http.StatusUnprocessableEntity, 422, nil, "手机号必须为 11位")
+		response.UnprocessableEntity(context, nil, "手机号必须为 11位")
 		return
 	}
 	if len(password) <= 6 {
-		response.Response(context, http.StatusUnprocessableEntity, 422, nil, "密码不能少于 6位")
+		response.UnprocessableEntity(context, nil, "密码不能少于 6位")
 		return
 	}
-
 	// 判断手机号是否注册
 	user, err := model.GetAUserByTelephone(telephone)
 	if err != nil || user.ID == 0 {
-		response.Response(context, http.StatusUnprocessableEntity, 422, nil, "手机号未注册")
+		response.UnprocessableEntity(context, nil, "手机号未注册")
 		return
 	}
 	// 判断密码是否正确
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
-		response.Fail(context, nil, "密码错误")
+		response.ServerError(context, nil, "密码错误")
 		log.Printf("token generate err: %v", err)
 		return
 	}
-
 	// 发放 token
 	token, err := common.ReleaseToken(user)
 	if err != nil {
-		response.Response(context, http.StatusInternalServerError, 500, nil, "生成 Token 失败")
+		response.Fail(context, nil, "生成 Token 失败")
 		return
 	}
-
 	// 返回结果
 	response.Success(context, &gin.H{"token": token}, "登录成功")
 }
 
 // 获取信息
 func Info(context *gin.Context) {
+	// 从上下文中读取 user 信息
 	user, _ := context.Get("user")
+	// 处理 dto 数据
 	dtoUser := dto.ToUserDto(user.(*model.User))
+	// 返回封装
 	response.Success(context, &gin.H{"user": dtoUser}, "")
 }
 
@@ -132,6 +112,5 @@ func isTelephoneExist(telephone string) bool {
 	if user.ID != 0 {
 		return true
 	}
-
 	return false
 }
--- response/response.go
+++ response/response.go
@@ -14,12 +14,29 @@ func Response(context *gin.Context, httpStatus int, code int, data *gin.H, msg s
 	})
 }
 
-// Response func Success
+// Response func 200 Success
 func Success(context *gin.Context, data *gin.H, msg string) {
 	Response(context, http.StatusOK, 200, data, msg)
 }
 
-// Response func Fail
+// Response func 400 Fail
 func Fail(context *gin.Context, data *gin.H, msg string) {
 	Response(context, http.StatusOK, 400, data, msg)
+}
+
+// Response func 422 Unprocessable Entity
+// 服务器理解请求实体的内容类型，并且请求实体的语法是正确的，但是服务器无法处理所包含的指令
+func UnprocessableEntity(context *gin.Context, data *gin.H, msg string) {
+	Response(context, http.StatusUnprocessableEntity, 422, data, msg)
+}
+
+// Response func 201 Created
+// 代表成功的应答状态码，表示请求已经被成功处理，并且创建了新的资源。
+func Created(context *gin.Context, data *gin.H, msg string) {
+	Response(context, http.StatusCreated, 201, data, msg)
+}
+
+// Response func 500 ServerError
+func ServerError(context *gin.Context, data *gin.H, msg string) {
+	Response(context, http.StatusInternalServerError, 500, data, msg)
 }
